be78553c9ca5f83dd0bdf667379e8eb44a5f5d41
mmarano25 <mmarano@uci.edu>
2019-02-28 09:39:42 -0800

    Wrote in basic flask to use flask dev server
        - backend/main.py uses vanilla Flask and parses input. Running the
        server now requires a couple bash commands, but it now reloads on
        any input and provides robust debugging information.
        - RecommendationResource is now a function and expects parsed input
        - Added docstrings and type hints where possible

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,36 @@
-from flask import Flask
-from flask_restful import Api
-from recommendation import RecommendationResource as rr
+"""
+Main flask script. Defines the '/recommendation/' route and parses input. 
+
+To start the flask server, run the following bash commands from the folder backend:
+    export FLASK_APP=main.py
+    export FLASK_ENV=development
+    flask run
+
+The server will automatically reload on any change to a file.
+"""
+# TODO: more robust query evaluation
+from flask import Flask, request, jsonify
+from recommendation.RecommendationResource import recommendResource
 
 app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(rr.RecommendationResource, '/recommendation/')
 
-if __name__ == '__main__':    
-    # Start the server
-    app.run(debug=True)
+@app.route("/recommendation/")
+def recommendation():
+    app.logger.info("Request: '{}'".format(request.args))
+
+    # Ensure request args are correct format
+    num_resources = int(request.args.get("num_resources"))
+    query = _response_to_float_list(request.args.get("query"))
+
+    # Get recommendation list
+    responseRec = recommendResource(num_resources=num_resources, query=query)
+    app.logger.info("Sending: '{}'".format(responseRec))
+
+    # Return json of recommendation list
+    return jsonify(responseRec)
+
+def _response_to_float_list(float_list: str) -> [float]:
+    str_list = float_list[1:-1].split(",")
+    return [float(x) for x in str_list]
+
