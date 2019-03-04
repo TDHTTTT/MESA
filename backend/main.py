97f112c61f1ce19c95215cd5e2a0919a4e8f2bee
mmarano25 <mmarano@uci.edu>
2019-03-04 10:42:26 -0800

    Finished Flask restructuring and updated Docker
    - RecommendationResource now is a public interface to the machine
    learning model, and training has been moved to a function
    - Training happens transparently before the first request to the Flask
    backend
    - New Docker entrypoint added that configures and runs Flask server in
    debugging mode.
    - Note: using ENTRYPOINT requires the new -t flag in the Docker run
    command to continue allowing ctrl-c to kill the container, README
    updated to reflect this.

--- backend/main.py
+++ backend/main.py
@@ -3,27 +3,42 @@ Main flask script. Defines the '/recommendation/' route and parses input.
 """
 # TODO: more robust query evaluation
 from flask import Flask, request, jsonify
-from recommendation.RecommendationResource import recommendResource
+from recommendation.RecommendationResource import initModel, recommendTasks
+
 
 app = Flask(__name__)
+app.before_first_request(initModel)
 
 
-@app.route("/recommendation/")
+@app.route("/recommendation/", methods=['GET'])
 def recommendation():
-    app.logger.info("Request: '{}'".format(request.args))
+    """
+    GET route that takes two parameters, hands them off to recommendation
+    algorithm, and sends back the result. 
+    Params:
+        num_resources: int
+        query: [float]
+
+    Returns:
+        JSON list of floats representing ranked recommendations
+
+    """
+    app.logger.debug("Request args: '{}'".format(request.args))
 
     # Ensure request args are correct format
     num_resources = int(request.args.get("num_resources"))
     query = _response_to_float_list(request.args.get("query"))
 
     # Get recommendation list
-    responseRec = recommendResource(num_resources=num_resources, query=query)
-    app.logger.info("Sending: '{}'".format(responseRec))
+    responseRec = recommendTasks(num_resources=num_resources, query=query)
+    app.logger.debug("Sending: '{}'".format(responseRec))
 
     # Return json of recommendation list
     return jsonify(responseRec)
 
+
 def _response_to_float_list(float_list: str) -> [float]:
+    """Converts a comma-separated string of list of floats to [float]"""
     str_list = float_list[1:-1].split(",")
     return [float(x) for x in str_list]
 
--- backend/recommendation/RecommendationResource.py
+++ backend/recommendation/RecommendationResource.py
@@ -1,17 +1,39 @@
+"""Public interface to machine learning recommendation modules."""
 from recommendation import model
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+
+CLASSIFIER = None
 
 
-# Trains model based on training data
-classifier = model.MLClassifier()
-X, Y = model.read_train_data("./data/data.csv")
-classifier.train(X,Y)
+def initModel() -> model.MLClassifier:
+    """
+    Trains model based on training data.
+    If model has already been trained, do nothing.
+    """
+    global CLASSIFIER
+    if CLASSIFIER is None:
+        logger.debug("Training model...")
+        CLASSIFIER = model.MLClassifier()
+        X, Y = model.read_train_data("./data/data.csv")
+        CLASSIFIER.train(X,Y)
+        logger.debug("Model trained successfully.")
 
 
-def recommendResource(num_resources: int, query: [float]) -> [float]:
-    """Gets recommendations from trained model based on query input"""
+def recommendTasks(num_resources: int, query: [float]) -> [float]:
+    """
+    Gets recommendations from the trained model based on query input.
+    Train classifier if it hasn't been initialized yet. 
+    """
+    # Train classifier if None
+    global CLASSIFIER
+    if CLASSIFIER is None:
+        initModel()
+        
     # Find recommendations
-    out = classifier.predict(query)
+    out = CLASSIFIER.predict(query)
 
     # Use context to sort recommendation
     sorted_out = np.argsort(-out)
