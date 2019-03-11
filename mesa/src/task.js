67710a992468a39a1a5c9b431da1c24e0086bf87
Jesse Chong <jec334@gmail.com>
2019-03-11 13:28:19 -0700

    Implemented basic UI + integrated Kaj's task list

---
+++ mesa/src/task.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+export class Task extends Component {
+    render() { // TODO make the task clickable.
+        return (
+            <View style={styles.taskitem}>
+                <Text>Task: {this.props.name}</Text>
+                <Button
+				  // onPress={onPressLearnMore}
+				  title="DONE"
+				  color="#ffd200"
+				  accessibilityLabel="Click here to complete the task."
+				/>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+  taskitem: {
+  },
+
+});
