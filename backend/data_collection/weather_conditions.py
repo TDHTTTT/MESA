68194fddf9aa063e69e228d3b1bbf359c022497e
mmarano25 <mmarano@uci.edu>
2019-03-13 22:12:11 -0700

    Added ability to get weather status and see if its raining
    - Found weather library and signed up for API key.
    - Note that the location is hardcoded to Irvine.
    - weather_conditions.py under backend/data_collection/ provides two
    functions to ascertain weather state:
        - get_status: returns a short string about the current weather e.g.
        Rain or Clear or Cloudy
        - is_okay_to_go_outside: currently returns True unless it's raining
        or snowing. We can consider other factors, but in the example of
        going for a run it seems too much up to individual preference.

---
+++ backend/data_collection/weather_conditions.py
@@ -0,0 +1,28 @@
+"""Simple interface to weather API."""
+import pyowm
+
+
+OWM_KEY = "de273f5fce576a683552bcf5f518c381"
+IRVINE_ID = 5359777
+
+
+# Initalize weather API with private key
+OWM = pyowm.OWM(OWM_KEY)
+
+
+def get_status() -> str:
+    """
+    Returns short status of current weather conditions e.g. Rain or Clear
+    """
+    observation = OWM.weather_at_id(IRVINE_ID)
+    return observation.get_weather().get_status()
+
+def is_okay_to_go_outside() -> bool:
+    """
+    Checks if it's raining or snowing outside.
+    This can be expanded to cover more factors if we want.
+    """
+    observation = OWM.weather_at_id(IRVINE_ID)
+    status = observation.get_weather().get_status()
+    return (status != "Rain" and status != "Snow")
+
